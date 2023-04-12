5d5f20a027fcbd172e2f86d5e33e9347475590a1
Théo Chalimbaud <theo.chalimbaud@gmail.com>
2023-04-12 21:29:35 +0200

    Initial commit

---
+++ src/main/java/net/shadapin/shadventuremod/ShadventureModClient.java
@@ -0,0 +1,11 @@
+package net.shadapin.shadventuremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShadventureModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
